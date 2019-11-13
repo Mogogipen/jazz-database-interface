f6e6b209e66582638af0ffb5135b4371114d1655
Matthew Hall <matthewhall@SUU.EDU>
2019-11-13 11:47:36 -0700

    added a console log to search button that outputs all values in left section

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 function searchBtnPress() {
     console.log("worked");
+    var clarinet = document.getElementById("clarinet").value;
+    console.log("clarinet: " + clarinet);
+    var soprano = document.getElementById("soprano").value;
+    console.log("soprano: " + soprano);
+    var alto = document.getElementById("alto").value;
+    console.log("alto: " + alto);
+    var tenor = document.getElementById("tenor").value;
+    console.log("tenor: " + tenor);
+    var trombone = document.getElementById("trombone").value;
+    console.log("trombone: " + trombone);
+    var trumpet = document.getElementById("trumpet").value;
+    console.log("trumpet: " + trumpet);
+    var piano = document.getElementById("piano").value;
+    console.log("piano: " + piano);
+    var bass = document.getElementById("bass").value;
+    console.log("bass: " + bass);
+    var guitar = document.getElementById("guitar").value;
+    console.log("guitar: " + guitar);
+    var drums = document.getElementById("drums").value;
+    console.log("drums: " + drums);
+    var e = document.getElementById("tempo");
+    var result = e.options[e.selectedIndex].text;
+    console.log("tempo: " + result);
+    var e = document.getElementById("moonLighter");
+    var result = e.options[e.selectedIndex].text;
+    console.log("moonLighter: " + result);
+    var e = document.getElementById("redDot");
+    var result = e.options[e.selectedIndex].text;
+    console.log("redDpt: " + result);
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,58 @@
 
 function searchBtnPress(){
     console.log("worked");
+
+    let clarinet = document.getElementById("clarinet").value;
+    console.log("clarinet: "+clarinet);
+
+
+    let soprano = document.getElementById("soprano").value;
+    console.log("soprano: "+soprano);
+
+
+    let alto = document.getElementById("alto").value;
+    console.log("alto: "+alto);
+
+
+    let tenor = document.getElementById("tenor").value;
+    console.log("tenor: "+tenor);
+
+
+    let trombone = document.getElementById("trombone").value;
+    console.log("trombone: "+trombone);
+
+
+    let trumpet = document.getElementById("trumpet").value;
+    console.log("trumpet: "+trumpet);
+
+
+    let piano = document.getElementById("piano").value;
+    console.log("piano: "+piano);
+
+
+    let bass = document.getElementById("bass").value;
+    console.log("bass: "+bass);
+
+
+    let guitar = document.getElementById("guitar").value;
+    console.log("guitar: "+guitar);
+
+
+    let drums = document.getElementById("drums").value;
+    console.log("drums: "+drums);
+
+
+    var e = document.getElementById("tempo");
+	var result = e.options[e.selectedIndex].text;
+    console.log("tempo: "+result);
+    
+
+    var e = document.getElementById("moonLighter");
+	var result = e.options[e.selectedIndex].text;
+    console.log("moonLighter: "+result);
+    
+
+    var e = document.getElementById("redDot");
+	var result = e.options[e.selectedIndex].text;
+	console.log("redDpt: "+result);
 }
