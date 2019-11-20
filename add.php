31e0cd42defb9c8f3a04ae28999070ee2ffed3b8
Matthew Hall <matthewhall@SUU.EDU>
2019-11-20 10:27:27 -0700

    fiddled with forms and put a few links in a php file that might be helpful to us

---
+++ add.php
@@ -0,0 +1,9 @@
+<?php
+
+made a file
+
+https://www.codeandcourse.com/how-to-connect-html-form-to-mysql-database-using-php-in-4-minutes/
+
+https://www.codeandcourse.com/how-to-display-data-from-mysql-database-into-html-table-using-php/
+?>
+
